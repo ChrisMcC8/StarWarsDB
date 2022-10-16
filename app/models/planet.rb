2102925220686a508f3d267ff2d34fec2578e462
ChrisMcC8 <chris.mccallum97@gmail.com>
2022-10-16 17:06:43 -0500

    added search

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,17 @@
 class Planet < ApplicationRecord
 
     validate :name, :orbital_period, :diameter, :climate, :terrain, :population
+
+    def self.search(search)
+        if search
+            planet_type = Planet.find_by(name: search)
+            if planet_type
+                self.where(planet_id: planet_type)
+            else 
+                @planets = Planet.all    
+            end
+        else 
+            @planets = Planet.all
+        end
+    end
 end
