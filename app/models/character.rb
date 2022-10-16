75d7cd7092ecbb55471038bbe118e64c9dcb5e1b
ChrisMcC8 <chris.mccallum97@gmail.com>
2022-10-16 17:06:37 -0500

    added search

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,16 @@
 class Character < ApplicationRecord
     validate :name, :mass, :birth_year, :planet, :species
+
+    def self.search(search)
+        if search
+            character_type = Character.find_by(name: search)
+            if character_type
+                self.where(character_id: character_type)
+            else 
+                @characters = Character.all    
+            end
+        else 
+            @characters = Character.all
+        end
+    end
 end
