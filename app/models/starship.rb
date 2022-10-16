c4b9b875d87695bab7b3d8010a577d80d18306a0
ChrisMcC8 <chris.mccallum97@gmail.com>
2022-10-16 17:06:56 -0500

    added search

--- app/models/starship.rb
+++ app/models/starship.rb
@@ -1,4 +1,17 @@
 class Starship < ApplicationRecord
 
     validate :name, :model, :manufacturer, :length, :crew, :passengers, :starship_class
+
+    def self.search(search)
+        if search
+            starship_type = Starship.find_by(name: search)
+            if starship_type
+                self.where(starship_id: starship_type)
+            else 
+                @starships = Starship.all    
+            end
+        else 
+            @startships = Starship.all
+        end
+    end
 end
